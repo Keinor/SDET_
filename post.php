b21d058b3e9a7e50fcdcd82cc1c61dbc0c99043e
Sally <33789976+Keinor@users.noreply.github.com>
2018-11-15 00:34:47 +0300

    Add files via upload

---
+++ post.php
@@ -0,0 +1,18 @@
+<?
+$name = $_POST["name"];
+$msg = $_POST["message"];
+$rem=$_POST["accept"];
+$mf= $_POST["radio"];
+
+
+//echo "<pre>";
+//print_r($_SERVER);
+//echo "</pre>";
+
+$a=$_SERVER['REQUEST_URI'];
+if($rem==1)
+    $check='Запомнить меня';
+
+ $msg1 = 'Имя: ' . $name .' '.' Сообщение: ' . $msg .' ;Пол: ' . $mf .' ;'.  $check;
+mail("1good771@gmail.com", "Вам пришло письмо с сайта $a", $msg1, "From: bright.instant@yandex.ru");
+?>
