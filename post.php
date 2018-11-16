df36502d813ef6ed2c8f3a4b4c943e33f34141f9
Sally <33789976+Keinor@users.noreply.github.com>
2018-11-16 22:12:14 +0300

    Update post.php

--- post.php
+++ post.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $name = $_POST["name"];
 $msg = $_POST["message"];
 $rem=$_POST["accept"];
@@ -7,7 +7,30 @@ $mf= $_POST["radio"];
 $a=$_SERVER['REQUEST_URI'];
 if($rem==1)
     $check='Запомнить меня';
+$to= '...@gmail.com';
+$subject = 'HoweWork';
+$message = '
+<table>
+	<th>Вам пришло письмо с сайта '.$a.'</th>
+	<tr>
+		<td>Имя</td> <td>'.$name.'</td>
+	</tr>
+	<tr>
+		<td>Сообщение</td> <td>'.$msg.'</td>
+	</tr>
+	<tr>
+		<td>Пол</td> <td>'.$mf.'</td>
+	</tr>
+	<tr>
+		<td>Состояние</td> <td>'.$check.'</dr>
+	</tr>
+ </table>
+ ';
 
- $msg1 = 'Имя: ' . $name .' '.' Сообщение: ' . $msg .' ;Пол: ' . $mf .' ;'.  $check;
-mail("1good771@gmail.com", "Вам пришло письмо с сайта $a", $msg1, "From: bright.instant@yandex.ru");
+// Для отправки HTML-письма должен быть установлен заголовок Content-type
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+mail($to, $subject, $message, $headers);
 ?>
+
