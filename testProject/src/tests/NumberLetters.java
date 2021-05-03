366f4b5aa40cc6c948120463ca47a9ad646d4ea2
Keinor <bright.instant@yandex.ru>
2021-05-03 23:48:26 +0300

    test

--- testProject/src/tests/NumberLetters.java
+++ testProject/src/tests/NumberLetters.java
@@ -6,8 +6,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.YandexPage;
+
 import java.util.concurrent.TimeUnit;
-import static helpers.Waiters.waitUntilLoad;
 
 public class NumberLetters {
     public static YandexPage yandexPage;
