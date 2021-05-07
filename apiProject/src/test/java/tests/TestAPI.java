faabd171038e7829817589b311de361ec0aba3c6
Keinor <bright.instant@yandex.ru>
2021-05-07 19:24:21 +0300

    update Test

--- apiProject/src/test/java/tests/TestAPI.java
+++ apiProject/src/test/java/tests/TestAPI.java
@@ -1,18 +1,37 @@
 package tests;
 
 import static io.restassured.RestAssured.*;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import static io.restassured.matcher.RestAssuredMatchers.*;
+import static io.restassured.path.json.JsonPath.from;
 import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.testng.annotations.Test;
 
 
 public class TestAPI {
 
     @Test
-    public void testGet() {
+    public Integer testGetPage() {
+        Response responce = get("https://reqres.in/api/users?page=1");
+        JsonPath extractor = responce.jsonPath();
+        return extractor.get("total_pages");
+    }
+
+    @Test
+    public void testGetUserOfPagination(){
+        String checkUser = null;
+        for (int i = 1; i <= testGetPage(); i++) {
+        String response = get("https://reqres.in/api/users?page={i}",i).asString();
+        checkUser  = from(response).get("data.find{it.first_name=='Michael' && it.last_name=='Lawson' }.email");
+    }
+        assertThat(checkUser, is("michael.lawson@reqres.in"));
+    }
+
+    @Test
+    public void testGetUser() {
         Response responce = get("https://reqres.in/api/users");
-       // System.out.println(responce.getBody().asString());
+
         given()
                 .get("https://reqres.in/api/users")
                 .then()
@@ -20,12 +39,5 @@ public class TestAPI {
                         equalTo("george.bluth@reqres.in" ));
     }
 
-    @Test
-    public void testGetPagination(){
-        given()
-                .get("https://reqres.in/api/users?page=2")
-                .then()
-                .body("data.find{it.first_name=='Michael' && it.last_name=='Lawson'}.email",
-                        equalTo("michael.lawson@reqres.in" ));
-    }
+
 }
