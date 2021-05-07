ce1e096b9c85f5e6bafad6b2ad743c2247276dea
Keinor <bright.instant@yandex.ru>
2021-05-07 22:40:42 +0300

    add helpers

--- apiProject/src/test/java/tests/TestAPI.java
+++ apiProject/src/test/java/tests/TestAPI.java
@@ -1,42 +1,40 @@
 package tests;
 
-import static io.restassured.RestAssured.*;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import static io.restassured.path.json.JsonPath.from;
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
 import org.testng.annotations.Test;
-
+import static helpers.HelperGet.testGetPage;
+import static io.restassured.RestAssured.given;
+import static io.restassured.path.json.JsonPath.from;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class TestAPI {
 
     @Test
-    public Integer testGetPage() {
-        Response responce = get("https://reqres.in/api/users?page=1");
-        JsonPath extractor = responce.jsonPath();
-        return extractor.get("total_pages");
-    }
-
-    @Test
-    public void testGetUserOfPagination(){
+    public void testGetUserOfPagination() {
         String checkUser = null;
+
         for (int i = 1; i <= testGetPage(); i++) {
-        String response = get("https://reqres.in/api/users?page={i}",i).asString();
-        checkUser  = from(response).get("data.find{it.first_name=='Michael' && it.last_name=='Lawson' }.email");
-    }
+            String response = given().
+                    param("page", i).
+                    get("https://reqres.in/api/users?{i}", i).asString();
+
+            checkUser = from(response).get("data.find{it.first_name=='Michael' && it.last_name=='Lawson' }.email");
+            if (checkUser != null) {
+                break;
+            }
+        }
         assertThat(checkUser, is("michael.lawson@reqres.in"));
     }
 
     @Test
     public void testGetUser() {
-        Response responce = get("https://reqres.in/api/users");
 
         given()
                 .get("https://reqres.in/api/users")
                 .then()
                 .body("data.find{it.first_name=='George' && it.last_name=='Bluth'}.email",
-                        equalTo("george.bluth@reqres.in" ));
+                        equalTo("george.bluth@reqres.in"));
     }
 
 
